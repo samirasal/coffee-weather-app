0dc145f76c5622883366cfd995339a2969d3fe47
samirasal <111319083+samirasal@users.noreply.github.com>
2022-08-23 12:53:22 -0400

    updated the weather icon to follow real time, as well as the description

--- src/app.js
+++ src/app.js
@@ -39,6 +39,12 @@ function showTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   let sundayElement = document.querySelector("#sunday");
   sundayElement.innerHTML = Math.round(response.data.main.temp);
